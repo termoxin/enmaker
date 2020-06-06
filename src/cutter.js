3795bd6f1cfa43ab1d307dfb2f63de5d16bae8b3
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-06-06 19:31:01 +0300

    Feature: add offset for cutter

--- src/cutter.js
+++ src/cutter.js
@@ -2,16 +2,17 @@ import path from "path";
 import ffmpeg from "fluent-ffmpeg";
 import chalk from "chalk";
 import { srtTimeToSeconds } from "parallelizer";
+const __dirname = path.resolve();
 
 import { toSnakeCase } from "./helpers/string";
 
 export const createOutPutPath = (filename, ext = ".mp4") =>
-  path.resolve(__dirname, "../output", `${toSnakeCase(filename)}${ext}`);
+  path.resolve(__dirname, "output", `${toSnakeCase(filename)}${ext}`);
 
-const cut = (filename, path, start, end) => {
-  const startTimeInSeconds = srtTimeToSeconds(start);
+const cut = (filename, path, start, end, offsetStart = 0, offsetEnd = 0) => {
+  const startTimeInSeconds = srtTimeToSeconds(start) - offsetStart;
 
-  const duration = srtTimeToSeconds(end) - startTimeInSeconds;
+  const duration = srtTimeToSeconds(end) - startTimeInSeconds + offsetEnd;
 
   return new Promise((resolve, reject) => {
     ffmpeg(path)
@@ -24,9 +25,23 @@ const cut = (filename, path, start, end) => {
   });
 };
 
-export const cutter = async (filename, path, start, end) => {
+export const cutter = async (
+  filename,
+  path,
+  start,
+  end,
+  offsetStart,
+  offsetEnd
+) => {
   if (filename) {
-    const result = await cut(filename, path, start, end);
+    const result = await cut(
+      filename,
+      path,
+      start,
+      end,
+      offsetStart,
+      offsetEnd
+    );
 
     console.log(
       chalk.green(
