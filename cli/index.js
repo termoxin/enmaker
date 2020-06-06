071cd498eb88cf2990b74126ef27d4de859845d9
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-06-06 12:28:30 +0300

    Feature: create CLI that has different modes (timestamp, phrase)

---
+++ cli/index.js
@@ -0,0 +1,61 @@
+import inquirer from "inquirer";
+
+import { runByTimestamp, runByPhrase } from "../src/index";
+
+const MODES = {
+  PHRASE: "Phrase",
+  TIMESTAMP: "Timestamp",
+};
+
+const QUESTIONS = {
+  SELECT_MODE: {
+    type: "list",
+    choices: Object.values(MODES),
+    name: "mode",
+    message: "Select cutting mode",
+  },
+  PHRASE: {
+    type: "input",
+    name: "phrase",
+    message: "Enter a phrase name",
+  },
+  FILENAME: {
+    type: "input",
+    name: "filename",
+    message: "Enter a file name",
+  },
+  START_TIME: {
+    type: "input",
+    name: "startTime",
+    message: "Enter a start time",
+  },
+  END_TIME: {
+    type: "input",
+    name: "endTime",
+    message: "Enter an end time",
+  },
+};
+
+const cli = async () => {
+  const questions = [];
+
+  const { mode } = await inquirer.prompt(QUESTIONS.SELECT_MODE);
+
+  if (mode === MODES.TIMESTAMP) {
+    questions.push(...Object.values(QUESTIONS));
+
+    const answers = await inquirer.prompt(questions);
+
+    await runByTimestamp(answers);
+  }
+
+  if (mode === MODES.PHRASE) {
+    questions.push(QUESTIONS.PHRASE);
+
+    const answers = await inquirer.prompt(questions);
+
+    await runByPhrase(answers);
+  }
+};
+
+cli();
