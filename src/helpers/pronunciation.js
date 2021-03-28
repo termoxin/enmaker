f878fdc0b0a081a60e37616db8adeca4e2b458c1
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-03-28 19:36:34 +0300

    Fix: no pronuncition for word

--- src/helpers/pronunciation.js
+++ src/helpers/pronunciation.js
@@ -1,16 +1,20 @@
 import fetch from "node-fetch";
 
 export const getPronunciation = async (word) => {
-  const response = await fetch(
-    `https://owlbot.info/api/v4/dictionary/${encodeURIComponent(word)}`,
-    {
-      headers: {
-        Authorization: `Token ${process.env.OWLBOT_TOKEN}`,
-      },
-    }
-  );
+  try {
+    const response = await fetch(
+      `https://owlbot.info/api/v4/dictionary/${encodeURIComponent(word)}`,
+      {
+        headers: {
+          Authorization: `Token ${process.env.OWLBOT_TOKEN}`,
+        },
+      }
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return data.pronunciation;
+    return data.pronunciation;
+  } catch (err) {
+    return "";
+  }
 };
