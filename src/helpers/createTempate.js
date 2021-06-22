d89f86ad9e548603b501e42b023503d0cc15c266
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-06-22 10:45:31 +0300

    Feature: add two extra spaces for transcription

--- src/helpers/createTempate.js
+++
@@ -1,6 +0,0 @@
-export const createTemplate = (phrase, pronunciation, transcripts) => `${phrase}${
-  phrase.split(" ").length === 1 ? ` |${pronunciation}|` : ""
-} —
-
-${transcripts}
-`;
---
+++ src/helpers/createTemplate.js
@@ -0,0 +1,10 @@
+export const createTemplate = (
+  phrase,
+  pronunciation,
+  transcripts
+) => `${phrase}${
+  phrase.split(" ").length === 1 ? ` | ${pronunciation} |` : ""
+} —
+
+${transcripts}
+`;
